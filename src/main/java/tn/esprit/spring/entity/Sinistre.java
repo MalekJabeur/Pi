3912effafdb06d8841ee698a7b7142c6cfb8b865
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-02 22:09:42 +0100

    update

--- src/main/java/tn/esprit/spring/entity/Sinistre.java
+++ src/main/java/tn/esprit/spring/entity/Sinistre.java
@@ -1,5 +1,55 @@
 package tn.esprit.spring.entity;
 
-public class Sinistre {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Sinistre implements Serializable{
+
+	@Id
+	private long Id_Sinistre;
+	@Temporal(TemporalType.DATE)
+	private Date Date_Declaration;
+	private String Type_Sinistre;
+	private String Place_Sinistre;
+	private String State_Sinistre;
+	
+	
+	public long getId_Sinistre() {
+		return Id_Sinistre;
+	}
+	public void setId_Sinistre(long id_Sinistre) {
+		Id_Sinistre = id_Sinistre;
+	}
+	public Date getDate_Declaration() {
+		return Date_Declaration;
+	}
+	public void setDate_Declaration(Date date_Declaration) {
+		Date_Declaration = date_Declaration;
+	}
+	public String getType_Sinistre() {
+		return Type_Sinistre;
+	}
+	public void setType_Sinistre(String type_Sinistre) {
+		Type_Sinistre = type_Sinistre;
+	}
+	public String getPlace_Sinistre() {
+		return Place_Sinistre;
+	}
+	public void setPlace_Sinistre(String place_Sinistre) {
+		Place_Sinistre = place_Sinistre;
+	}
+	public String getState_Sinistre() {
+		return State_Sinistre;
+	}
+	public void setState_Sinistre(String state_Sinistre) {
+		State_Sinistre = state_Sinistre;
+	}
+	
+	
 }
